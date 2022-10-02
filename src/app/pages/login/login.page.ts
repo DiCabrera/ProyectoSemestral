f49baeedf67ac4a03fc31eda09b65beb4d186ab2
DiCabrera <diegoc1739@gmail.com>
2022-10-02 13:35:49 -0300

    segundoCommit
    Validación del login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+ 
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  pageTitle = 'login';
+  Visible = true;
+
+  //Model
+  user : any ={
+    username: '',
+    password: '',
+  }
+
+  field: string = '';
+
+  constructor(private toastCtrl: ToastController, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    if(this.validateModel(this.user)){
+    
+      if(this.user.username =='ADMIN' && this.user.password =='admin'){
+        this.presentToast('Bienvenido ' + this.user.username);
+        this.router.navigate(['/']);
+      }
+
+      else if(this.user.username =='USER' && this.user.password =='user'){
+        this.presentToast('Bienvenido ' + this.user.username);
+        this.router.navigate(['/']);
+      }
+
+      else{
+        this.presentToast('Los datos ingresados son invalidos');
+      };
+    }
+
+    else{
+      this.presentToast('Debes ingresar: ' + this.field);
+    }
+  }
+   
+  validateModel(model: any){
+    for(var[key,value] of Object.entries(model)){
+      if(value == ''){
+        this.field = key;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  async presentToast(message: string, duration?: number){
+    const toast = await this.toastCtrl.create({
+      message:message,
+      duration:duration?duration:2000
+    });
+    toast.present();
   }
 
 }
