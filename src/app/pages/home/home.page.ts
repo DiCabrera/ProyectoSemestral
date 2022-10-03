1a97e6c6f604cc71770431bd9baeeb2c6c65bca5
Nicolas Miranda <ni.mirandan@duocuc.cl>
2022-10-03 19:50:02 -0300

    agregue page/welcome, componente cargarLoadingmodifique el menu lo agregue a home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  public appPages = [
+    { title: 'Bienvenida', url: '/welcome', icon: 'mail' },
+    { title: 'conversor', url: '/', icon: 'paper-plane' },
+    { title: 'about', url: '/', icon: 'heart' }
+  ];
   constructor() { }
 
   ngOnInit() {
