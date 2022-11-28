0b24009c24f0fee9b155e585bb7bfc4175d64c0d
Nicolas Miranda <ni.mirandan@duocuc.cl>
2022-11-28 17:47:28 -0300

    add component conversor de monedas euro y dolar a clp

--- src/app/pages/conversor/conversor.page.ts
+++ src/app/pages/conversor/conversor.page.ts
@@ -27,6 +27,16 @@ export class ConversorPage implements OnInit {
   dataEuroAPeso = null;
   dataDolar = null;
   dataDolarAPeso = null;
+  chileno=1;
+  chilenopesos=0;
+  monedaEuro: any = {
+    euro: 0,
+  }
+  monedaDolar: any = {
+    dolar: 0,
+  }
+  
+  field: string = '';
   getDat:DataInterface = {
    serie: []
   };
@@ -37,7 +47,7 @@ export class ConversorPage implements OnInit {
       const valor = this.getDat.serie[0].valor;
       console.log({ valor });
       this.dataEuro = valor;
-      this.dataEuroAPeso = valor/100;
+      this.dataEuroAPeso = valor;
     })
     this._services.getDataDolar<any[]>("").subscribe(data => {
       this.getDat = data as unknown as DataInterface;
@@ -45,11 +55,29 @@ export class ConversorPage implements OnInit {
       const valor = this.getDat.serie[0].valor;
       console.log({ valor });
       this.dataDolar = valor;
-      this.dataDolarAPeso = valor/100;
+      this.dataDolarAPeso = valor;
 
 
     })
   }
+  
+  clickConvertEuro(){
+    this.monedaEuro.euro = (this.monedaEuro.euro*944.35);
+    
+    console.log(this.monedaEuro.euro);
+    console.log("euro");
+    console.log(this.chileno);
+    console.log("peso");
+  }
+  clickConvertDolar(){
+    this.monedaDolar.dolar = (this.monedaDolar.dolar*913.45);
+    
+    console.log(this.monedaDolar.dolar);
+    console.log("euro");
+    console.log(this.chileno);
+    console.log("peso");
+  }
+
   ngOnInit() {
   }
 
