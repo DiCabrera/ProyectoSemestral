81191a67842d419c18d5dc754b628d12d6986c3a
Nicolas Miranda <ni.mirandan@duocuc.cl>
2022-11-27 22:58:51 -0300

    get of the conversor

--- src/app/pages/conversor/conversor.page.ts
+++ src/app/pages/conversor/conversor.page.ts
@@ -2,27 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { GetapiService } from 'src/app/services/getapi.service';
 
 
+interface SerieInterface {
+  fecha: string;
+  valor: number;
+}
+
+interface DataInterface {
+  serie: SerieInterface[];
+}
+
+
 @Component({
   selector: 'app-conversor',
   templateUrl: './conversor.page.html',
   styleUrls: ['./conversor.page.scss'],
 })
+
+
 export class ConversorPage implements OnInit {
 
   pageTitle = 'Conversor';
   isNotLogin = true;
-  getDat: any[] = [];
+  dataEuro = null;
+  dataEuroAPeso = null;
+  dataDolar = null;
+  dataDolarAPeso = null;
+  getDat:DataInterface = {
+   serie: []
+  };
   constructor(public _services: GetapiService) {
 
     this._services.getDataEuro<any[]>("").subscribe(data => {
-      this.getDat = data;
-      console.log(this.getDat)
-
-
+      this.getDat = data as unknown as DataInterface;
+      const valor = this.getDat.serie[0].valor;
+      console.log({ valor });
+      this.dataEuro = valor;
+      this.dataEuroAPeso = valor/100;
     })
-    this._services.getDataEuro<any[]>("").subscribe(data => {
-      this.getDat = data;
-      console.log(this.getDat)
+    this._services.getDataDolar<any[]>("").subscribe(data => {
+      this.getDat = data as unknown as DataInterface;
+      this.getDat = data as unknown as DataInterface;
+      const valor = this.getDat.serie[0].valor;
+      console.log({ valor });
+      this.dataDolar = valor;
+      this.dataDolarAPeso = valor/100;
 
 
     })
